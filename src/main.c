386035e6e94e024a5bbc3c9635142074f291a913
Mike Quin <mike@elite.uk.com>
2025-08-05 23:56:26 +0100

    Add buffering and Telnet protocol support.
    Keep reading from the UART until it reports there is no more data,
    populating a buffer which is printed from once the UART is idle.
    
    Handle Telnet <IAC> characters and do naive Telnet negotiation -
    enable ECHO and reject anything else the server offers or asks for.

--- src/main.c
+++ src/main.c
@@ -9,30 +9,81 @@
 
 void main(int argc, char *argv[])
 {
+    unsigned char buffer[2048];
+    unsigned int bpos=0;
+
     if (argc < 3) {
         printf("Usage:\n%s host port\n", argv[0]);
         
         return;
     }
 
-    int c, k;
+    unsigned char c, k, f, q, t, status;
+    unsigned int i;
+    q=0;
+
     uart_init();
     printf("Uart inited\r\n");
     printf("CTRL-Q for exit\r\n");
 
     if (wifi_connect(argv[1], argv[2]))
         for (;k!=17;) {
-            for (c = uart_getc(); c != -1 ; c = uart_getc()) 
-                if (c != -1) printf("%c", c);
-            
-            k = getk();
-            if (k) {
-                uart_putc(k);
-                if (k==13) 
-                    uart_putc(10);
-                while(getk() == k);
+       status = inp(UART_GetStatus);
+        while (status & UART_DATA_READY != 0) {
+            buffer[bpos]=inp(UART_RxD);
+            buffer[bpos+1]=0;
+            bpos++;
+            status = inp(UART_GetStatus);
+        }
+
+        if (bpos !=0) {
+            for (i=0 ; i < bpos ; i++) {
+                c=buffer[i];
+
+                if (c == 255 && q==0) {
+                        q=1;
+                        continue;
+                }
+                else if (c > 250 && q==1) {
+                    t=c;
+                    q=2;
+                    continue;
+                }
+                else if (q==2 && ( t==251 || t==252 )) {
+                    if ( c== 1 ) {
+                        uart_putc(255);
+                        uart_putc(253);
+                        uart_putc(c);
+                    } else {
+                        uart_putc(255);
+                        uart_putc(254);
+                        uart_putc(c);
+                    }
+                    q=0;
+                    continue;
+                }
+                else if (q==2 && ( t==253 || t==254 )) {
+                        uart_putc(255);
+                        uart_putc(252);
+                        uart_putc(c);
+                        q=0;
+                        continue;
+                } else {
+                   printf("%c",c);
+                }
             }
         }
+        buffer[0]=0;
+        bpos=0;
+
+        k = getk();
+        if (k) {
+            uart_putc(k);
+//                if (k==13)g
+//                    uart_putc(10);
+            while(getk() == k);
+        }
+    }
     reset_esp();
     
     return ;
